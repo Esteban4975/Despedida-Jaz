6581016fb8102c0cdc56ea59f363830d7afe0b43
Esteban-Valverde <esteban.valverde@xira.ai>
2021-10-29 11:37:27 -0500

    m

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <title>Despedida de soltera Jaz</title>
+        <div class="container">
+          <div class="row mt-5">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/P1fFT3bBiVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
       </header>
     </div>
   );
